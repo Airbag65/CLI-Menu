4746647ac9ae89d7f241cfb41e6c44e0fdfb4b8f
Anton Norman <antno464@student.liu.se>
2024-05-29 13:02:31 +0200

    1.0.0

--- src/airbagclimenu/Menu.py
+++ src/airbagclimenu/Menu.py
@@ -23,8 +23,9 @@ class MenuOption:
             raise IllegalArgumentError("Argument must be of type 'Function'")
         self.menu_options[len(self.menu_options) + 1] = [title, functionality]
 
-    def run(self):
-        os.system("cls" if os.name == "nt" else "clear")
+    def run(self, clear_screen = True):
+        if clear_screen:
+            os.system("cls" if os.name == "nt" else "clear")
         for key, val in self.menu_options.items():
             if isinstance(val, MenuOption):
                 print(f"[{key}] {val.title}")
@@ -36,13 +37,14 @@ class MenuOption:
             return
         chosen_option = self.menu_options[choice]
         if isinstance(chosen_option, MenuOption):
-            chosen_option.run()
+            chosen_option.run(clear_screen)
         else:
             chosen_option[1]()
 
 
 class CLIMenu:
-    def __init__(self):
+    def __init__(self, quit_stmt = True):
+        self.quit_stmt = quit_stmt
         self.menu_options = {}
 
     def add_menu_option(self, menu_option: MenuOption) -> None:
@@ -55,25 +57,51 @@ class CLIMenu:
             raise IllegalArgumentError("Argument must be of type 'Function'")
         self.menu_options[len(self.menu_options) + 1] = [title, functionality]
 
-    def run(self) -> None:
-        self.menu_options[len(self.menu_options) + 1] = ("Quit", "")
-        os.system("cls" if os.name == "nt" else "clear")
-        for key, val in self.menu_options.items():
-            if isinstance(val, MenuOption):
-                print(f"[{key}] {val.title}")
-            else:
-                print(f"[{key}] {val[0]}")
-        choice = int(input(": "))
-        if choice not in self.menu_options.keys():
-            print("Not a valid option")
-            return
-        chosen_option = self.menu_options[choice]
-        if isinstance(chosen_option, MenuOption):
-            chosen_option.run()
-        elif isinstance(chosen_option, tuple):
-            print("Quitting")
-            return
+    def run(self, clear_screen = True) -> None:
+        if self.quit_stmt:
+            self.menu_options[len(self.menu_options) + 1] = ("Quit", "")
+            _quit = self.quit_stmt
+            while _quit:
+                if clear_screen:
+                    os.system("cls" if os.name == "nt" else "clear")
+                for key, val in self.menu_options.items():
+                    if isinstance(val, MenuOption):
+                        print(f"[{key}] {val.title}")
+                    else:
+                        print(f"[{key}] {val[0]}")
+                choice = int(input(": "))
+                if choice not in self.menu_options.keys():
+                    print("Not a valid option")
+                    _quit = False
+                chosen_option = self.menu_options[choice]
+                if isinstance(chosen_option, MenuOption):
+                    chosen_option.run(clear_screen)
+                elif isinstance(chosen_option, tuple):
+                    print("Quitting")
+                    _quit = False
+                else:
+                    chosen_option[1]()
         else:
-            chosen_option[1]()
+            if clear_screen:
+                os.system("cls" if os.name == "nt" else "clear")
+            for key, val in self.menu_options.items():
+                if isinstance(val, MenuOption):
+                    print(f"[{key}] {val.title}")
+                else:
+                    print(f"[{key}] {val[0]}")
+            choice = int(input(": "))
+            if choice not in self.menu_options.keys():
+                print("Not a valid option")
+                return
+            chosen_option = self.menu_options[choice]
+            if isinstance(chosen_option, MenuOption):
+                chosen_option.run(clear_screen)
+            elif isinstance(chosen_option, tuple):
+                print("Quitting")
+                return
+            else:
+                chosen_option[1]()
+
+
 
 
